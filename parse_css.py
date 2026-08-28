352d064f6cc9dd8cbd60b30303637fba4ad8d74d
kanatipgittyh <kanatip.su@gmail.com>
2026-08-28 15:57:49 +0700

    go

--- parse_css.py
+++
@@ -1,9 +0,0 @@
-import re
-
-css = open('styles.css').read()
-# Find all @media (max-width: 768px) blocks
-matches = re.finditer(r'@media \(max-width: 768px\) \{([\s\S]*?)\n\}', css)
-for i, match in enumerate(matches):
-    print(f"--- Block {i} ---")
-    print(match.group(1))
-
